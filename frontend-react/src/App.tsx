ff3583cbc822400cef87ee7b7516de008ccac08d
Văn Lộc <chuvanloc1008@gmail.com>
2026-05-05 21:06:28 +0700

    Cài đặt react-vite, tailwincss và cấu trúc thư mục cho frontend

---
+++ frontend-react/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600 underline">
+        Tailwind CSS v4 đã sẵn sàng!
+      </h1>
+    </div>
+  )
+}
+
+export default App
